1f170bba259255cd220a3d4979488edcd64a57b3
Harry Lee <h344lee@uwaterloo.ca>
2025-06-24 17:52:53 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
@@ -6,19 +5,13 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-# Set the page configuration for the Streamlit app
-st.set_page_config(
-    page_title="Google Gemini Models by Harry",
-    page_icon="🤖"
-)
-
 # Get the Google API key from the environment variables
 api_key = st.sidebar.text_input("Gemini API Key", type="password")
 
 # Configure the Google Generative AI with the API key
 genai.configure(api_key=api_key)
 
-st.title("🦜🔗 Harry's Gemini Quickstart App")
+st.title("🔗Harry's Gemini Quickstart App")
 
 model = genai.GenerativeModel('gemini-2.5-flash')
 
